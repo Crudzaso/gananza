133f5add910e79936efca163ab46a147d36f589f
alejandroava <alejandroalzate.ava@gmail.com>
2024-11-05 12:00:19 -0500

    add: route, view, an link of route in welcome

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Acerca de Nosotros</h1>
+        <p>Esta es la página de "Acerca de Nosotros".</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "About",
+};
+</script>
