63c238e3252247b6f4617ce6e9be5772c8c35e77
DiegoAndresMejia <diegomejiasobsu@gmail.com>
2024-11-05 07:38:30 -0500

    add nullable options in the migration to the user model

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
-            $table->string('lastname');
-            $table->string('phone_number');
-            $table->string('document')->unique();
-            $table->string('document_type');
+            $table->string('lastname')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('document')->unique()->nullable();
+            $table->string('document_type')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
