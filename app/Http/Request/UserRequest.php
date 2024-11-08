d9515010a55a7b93b13e98df40bf185ba2cc17c6
BUKODONOZOR <jafetdavidibarguenlozano@pascualbravo.edu.co>
2024-11-07 22:36:41 -0500

    create the api and for users

---
+++ app/Http/Request/UserRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'lastname' => 'required|string|max:255',
+            'document' => 'required|string|unique:users,document,' . $this->user,
+            'document_type' => 'required|string',
+            'phone_number' => 'nullable|string',
+            'email' => 'required|string|email|max:255|unique:users,email,' . $this->user,
+            'password' => 'sometimes|required|string|min:8',
+            'google_id' => 'nullable|string|unique:users,google_id,' . $this->user,
+            'github_id' => 'nullable|string|unique:users,github_id,' . $this->user,
+        ];
+    }
+}
