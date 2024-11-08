d101ef62001e4e5fa897b447618d09772297894f
BUKODONOZOR <jafetdavidibarguenlozano@pascualbravo.edu.co>
2024-11-07 22:37:17 -0500

    Add Dashboard Components

---
+++ resources/js/Components/BallotWidget.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="ballot-widget">
+    <div 
+      v-for="(digit, index) in digits" 
+      :key="index" 
+      class="ball-container" 
+      :style="{ animationDelay: `${index * 0.2}s` }"
+    >
+      {{ digit }}
+    </div>
+    <button @click="generateRandomNumber">Generar Número</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const digits = ref([0, 0, 0, 0]); // Array para almacenar los números de cada "bola"
+
+function generateRandomNumber() {
+  digits.value = Array.from({ length: 4 }, () => Math.floor(Math.random() * 10)); // Genera números aleatorios entre 0 y 9 para cada dígito
+}
+</script>
+
+<style scoped>
+.ballot-widget {
+  display: flex;
+  gap: 15px;
+  background-color: rgb(0, 0, 0);
+  border-radius: 20px;
+  justify-content: center;
+  align-items: center;
+  margin-top: -7%;
+}
+
+.ball-container {
+  width: 60px;
+  height: 60px;
+ 
+  background: #ffdd00; /* Color de fondo */
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  color: white;
+  font-weight: bold;
+  animation: spin 1s ease-in-out;
+}
+
+/* Animación para simular la rotación */
+@keyframes spin {
+  0% { transform: rotateX(0deg); }
+  100% { transform: rotateX(360deg); }
+}
+
+button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  background-color: #ffdd00;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+}
+
+button:hover {
+  background-color: #ffdd00;
+}
+</style>
---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import SearchBar from './SearchBar.vue';
+import ButtonHeader1 from './ButtonHeader1.vue';
+import ButtonHeader2 from './ButtonHeader2.vue';
+
+const isScrolled = ref(false);
+
+const handleScroll = () => {
+  isScrolled.value = window.scrollY > 40;
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll);
+});
+</script>
+
+<template>
+  <header :class="['header', { 'header-scrolled': isScrolled }]">
+    <div class="title">
+      <h1>Dashboard</h1>
+      <p>Create a store with #YDR-124-346 code</p>
+    </div>
+
+    <div class="container-btns">
+      <SearchBar/>
+      <ButtonHeader1/>
+      <ButtonHeader2/>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+.header {
+  position: fixed;
+  top: 0;
+  left:13.4%;
+  width: 87%;
+  background-color: white !important;
+  border: 1px solid #ffdd00 ;
+  border-left: none ;
+  color: white;
+  padding: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 1000;
+  transition: height 0.2s ease; /* Transición suave para el cambio de altura */
+  height: 30%; /* Altura inicial */
+}
+
+.header-scrolled {
+  height: 19%; /* Altura reducida cuando se hace scroll */
+}
+
+.title {
+  margin-left: 18%;
+  color: black;
+}
+
+.title h1 {
+  margin: 0;
+  font-size: 24px;
+}
+
+.title p {
+  margin: 0;
+  font-size: 12px;
+  color: #c0c0d5;
+}
+
+.container-btns {
+  display: flex;
+  width: 40%;
+  height: 6vh;
+  margin-left: 20%;
+}
+</style>
