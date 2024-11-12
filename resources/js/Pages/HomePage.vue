2d00cbe1a0d89ac45404af5bffd4c5c16428dac5
alejandroava <alejandroalzate.ava@gmail.com>
2024-11-11 19:08:16 -0500

    fix:delete use of dashboard asidebar

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -5,7 +5,6 @@
 </template>
 <script>
 import { router } from '@inertiajs/vue3';
-import Aside from '@/Components/Dashboard/Aside.vue';
 import NavBar from '@/Components/Dashboard/NavBar.vue';
 
 export default {
